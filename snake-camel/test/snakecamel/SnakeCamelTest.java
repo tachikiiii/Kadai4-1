04f58319ee83aa35780269ba528dff1a56c720c3
g13948th <g13948th@s30527.tsuda.ac.jp>
2016-05-06 15:16:32 +0900

    変更

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -5,13 +5,29 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 	
 public class SnakeCamelTest {
+	SnakeCamelUtil snakecamel = new SnakeCamelUtil();
 
 	@Test
-	public void test() {
-		SnakeCamelUtil snakecamel = new SnakeCamelUtil();
+	public void testSnaketoCamelcase() {
+		String expected = "AbcDefGhi";
+		String actual = snakecamel.capitalize("abc_def_ghi");
+		assertThat(actual,is(expected));
+	}
+	public void testCamelToSnakecase() {
+		String expected = "abc_def_ghi";
+		String actual = snakecamel.capitalize("AbcDefGhi");
+		assertThat(actual,is(expected));
+	}
+
+	public void testCapitalize() {
 		String expected = "A";
 		String actual = snakecamel.capitalize("a");
 		assertThat(actual,is(expected));
 	}
 
+	public void testUncapitalize() {
+		String expected = "a";
+		String actual = snakecamel.uncapitalize("A");
+		assertThat(actual,is(expected));
+	}
 }
